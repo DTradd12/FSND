1d87a922bcf5ff9fbd95a84bd0af5dd5a7ff57b7
DTradd12 <derektradd@gmail.com>
2020-04-29 15:15:01 -0400

    added  routes

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -1,27 +1,149 @@
-import os
-from flask import Flask, request, abort, jsonify
+from flask import request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
-from models import setup_db, db_drop_and_create_all
+from models import create_app, setup_db, db_drop_and_create_all, Movie, Actor
+
 
 database_path = "postgresql://postgres:password@localhost:5432/castingagency"
 
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = database_path
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    CORS(app)
-
-    return app
-
-
-APP = create_app()
+APP = create_app(database_path)
 db = SQLAlchemy(APP)
 database = setup_db(APP)
-db_drop_and_create_all()
+# db_drop_and_create_all()
 
 
+@APP.route("/")
+def index():
+    movies = Movie.query.order_by(Movie.id)
+    actors = Actor.query.order_by(Actor.id)
+    movie_list = [movie.formatted() for movie in movies]
+    actor_list = [actor.formatted() for actor in actors]
+
+    return jsonify({
+        "Movies": movie_list,
+        "Actors": actor_list
+    })
+
+
+@APP.route("/actors", methods=['GET'])
+def get_actors():
+    actors = Actor.query.order_by(Actor.id)
+    actor_list = [actor.formatted() for actor in actors]
+
+    return jsonify({
+        "Actors": actor_list
+    })
+
+
+@APP.route("/movies", methods=['GET'])
+def get_movies():
+    movies = Movie.query.order_by(Movie.id)
+    movie_list = [movie.formatted() for movie in movies]
+
+    return jsonify({
+        "Movies": movie_list
+    })
+
+
+@APP.route("/actors/<int:actor_id>", methods=['GET'])
+def get_actor(actor_id):
+    actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
+
+    if actor is None:
+        return jsonify({
+          "Actors": "No actors are currently present in the database."
+        })
+    else:
+        return jsonify(actor.formatted())
+
+
+@APP.route("/movies/<int:movie_id>", methods=['GET'])
+def get_movie(movie_id):
+    movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
+
+    return jsonify(movie.formatted())
+
+
+@APP.route("/actors/<int:actor_id>", methods=['DELETE'])
+def delete_actor(actor_id):
+    actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
+    actor.delete()
+
+    return jsonify({
+        "Actor": actor.name,
+        "Deleted": True
+    })
+
+
+@APP.route("/movies/<int:movie_id>", methods=['DELETE'])
+def delete_movie(movie_id):
+    movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
+    movie.delete()
+
+    return jsonify({
+        "Movie": movie.name,
+        "Deleted": True
+    })
+
+
+@APP.route("/movies/create", methods=['POST'])
+def add_movie():
+    body = request.get_json()
+
+    new_movie = Movie(body['title'], body['release_date'])
+    new_movie.create()
+
+    return jsonify(new_movie.formatted())
+
+
+@APP.route("/actors/create", methods=['POST'])
+def add_actor():
+    body = request.get_json()
+
+    new_actor = Actor(body['name'], body['age'], body['gender'])
+    new_actor.create()
+
+    return jsonify(new_actor.formatted())
+
+
+@APP.route("/movies/<int:movie_id>", methods=['PATCH'])
+def edit_movie(movie_id):
+    body = request.get_json()
+
+    movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
+
+    movie.title = body['title']
+    movie.release_date = body['release_date']
+
+    movie.update()
+
+    return jsonify({
+        "updated": True,
+        "movie": movie.formatted()
+    })
+
+
+@APP.route("/actors/<int:actor_id>", methods=['PATCH'])
+def edit_actor(actor_id):
+    body = request.get_json()
+
+    actor = Actor.query.filter(Actor.id == actor_id).one_or_none()
+
+    actor.name = body['name']
+    actor.age = body['age']
+    actor.gender = body['gender']
+
+    actor.update()
+
+    return jsonify({
+        "updated": True,
+        "actor": actor.formatted()
+    })
+# Default port:
 # if __name__ == '__main__':
-#     APP.run(host='0.0.0.0', port=8080, debug=True)
+#     APP.run()
+
+# Or specify port manually:
+# if __name__ == '__main__':
+#     port = int(os.environ.get('PORT', 7000))
+#     APP.run(host='localhost', port=port)
--- projects/capstone/starter/models.py
+++ projects/capstone/starter/models.py
@@ -1,8 +1,6 @@
-import os
-from sqlalchemy import Column, String, Integer
+from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-import json
-
 
 db = SQLAlchemy()
 
@@ -17,18 +15,73 @@ def db_drop_and_create_all():
     db.create_all()
 
 
-class Movies(db.Model):
+def create_app(database_path):
+    # create and configure the app
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_path
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['JSON_SORT_KEYS'] = False
+    CORS(app)
+
+    return app
+
+
+class Movie(db.Model):
+    __table_name__ = 'movie'
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, unique=True, nullable=False)
-    release_Date = db.Column(db.String, nullable=False)
+    release_date = db.Column(db.DateTime())
+
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_Date = release_date
+
+    def formatted(self):
+        return {
+            "Title": self.title,
+            "Release Date": self.release_date
+        }
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
 
 
-class Actors(db.Model):
+class Actor(db.Model):
+    __table_name = 'actor'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True, nullable=False)
     age = db.Column(db.Integer, nullable=False)
-    gender = db.Column(db.String,nullable=False)
+    gender = db.Column(db.String, nullable=False)
 
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age,
+        self.gender = gender
 
+    def formatted(self):
+        return {
+            "Name": self.name,
+            "Age": self.age,
+            "Gender": self.gender
+        }
 
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
 
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
